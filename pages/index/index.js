726d68a916d349accd158ed5c7b05c04e2916071
wang rui <wangrui.0630@outlook.com>
2018-06-12 23:02:07 +0800

    continue pull refresh

---
+++ pages/index/index.js
@@ -0,0 +1,54 @@
+//index.js
+
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+};
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+};
+
+Page({
+  data: {
+    nowTemp: ``,
+    nowWeather: ``,
+    nowWeatherBackground: ``
+  },
+
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '广州市'
+      },
+      success: res => {
+        console.log(res)
+        let result = res.data.result;
+        let temp = result.now.temp;
+        let weather = result.now.weather;
+        console.log(temp, weather);
+
+        this.setData(
+          {
+            nowTemp: temp + "°",
+            nowWeather: weatherMap[weather],
+            nowWeatherBackground: '/images/' + weather + '-bg.png'
+          }
+        )
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+      }
+    })
+  }
+})
